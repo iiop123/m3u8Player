e164c3654cb4911b2ee170a0637e568f00a352bd
iiop123 <102747168+iiop123@users.noreply.github.com>
2024-01-17 14:32:40 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script>
+import Hls from 'hls.js'
+import DPlayer from 'dplayer';
+export default { 
+  data() {
+    return {
+      link:'https://s3plus.meituan.net/v1/mss_550586ef375b493da4aa79bebdfce4fa/csc-apply-file-web/prod/2023-02-03/e71eb453-4362-497c-a03a-026ce82fa7fd.jpg'
+    }
+  },
+  mounted() {
+    let location=(new URL(document.location).searchParams).get('url')
+if (location!=null) {
+  let name=location
+  this.link=name
+  console.log(`video link is ${name}`);
+}
+
+    this.dp= new DPlayer({
+    container: document.getElementById('dplayer'),
+    video: {
+        url: this.link,
+        type: 'customHls',
+        customType: {
+            customHls: function (video, player) {
+                const hls = new Hls();
+                hls.loadSource(video.src);
+                hls.attachMedia(video);
+            },
+        },
+    },
+});
+
+  },
+  methods: {
+    update(){
+      this.dp= new DPlayer({
+    container: document.getElementById('dplayer'),
+    video: {
+        url: this.link,
+        type: 'customHls',
+        customType: {
+            customHls: function (video, player) {
+                const hls = new Hls();
+                hls.loadSource(video.src);
+                hls.attachMedia(video);
+            },
+        },
+    },
+});
+return this.dp.play()
+    }
+  },
+}
+</script>
+<template>
+  
+  <nut-cell style="position:relative;top:20px;justify-content: center;">
+    <nut-input :border="false" style="width:80%;border:solid 2px;" type='text' clear-size='18px' @keyup.enter="update"	placeholder="请输入m3u8链接" clearable v-model="this.link"/>
+      <nut-button shape="square" type="primary" style="height:45px" @click="update">点击播放</nut-button>
+    
+  </nut-cell>
+  <main style="position:relative;top:20px;" class="flex_center">
+  <div id="dplayer"></div>
+  </main>
+  </template>
+<style>
+.flex_center{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.nut-input-clear.nut-icon{
+  border:none!important
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import nutui from '@nutui/nutui'
+import "@nutui/nutui/dist/style.css";
+createApp(App).use(nutui).mount('#app')
